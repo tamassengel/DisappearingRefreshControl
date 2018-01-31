0f7f1a294387e4211655e56a69c7f1ff637fdb73
Sengel Tamás <the4kman@gmail.com>
2018-01-31 14:56:43 +0100

    Initial Commit

---
+++ DisappearingRefreshControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DisappearingRefreshControl
+//
+//  Created by Sengel Tamás on 1/31/18.
+//  Copyright © 2018 the4kman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
